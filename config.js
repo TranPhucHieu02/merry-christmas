071215dddb4efeb6b7cb19eddec62234692ca790
Hiếu Trần Phúc <phuchieu20.01@gmail.com>
2023-12-17 12:13:37 +0700

    update 2

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ const eventName = 'Merry Christmas';
 const titleCard = 'Tặng người ấy';
 const contentCard = 'Tặng em niềm vui để em mỉm cười mỗi ngày, tặng em nụ cười để luôn rạng ngời và gửi tặng em những điều tốt đẹp nhất. Chúc em yêu Giáng sinh ấm áp và an lành.';
 
-const giftImage = 'hot-girl.jpg';
+const giftImage = 'pic.jpg';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
