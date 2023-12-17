056e8c7eb19b8fbccfab7f6ae0f772f2cb77ab59
Hiếu Trần Phúc <phuchieu20.01@gmail.com>
2023-12-17 11:05:21 +0700

    create card

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Em';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Merry Christmas';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Tặng em niềm vui để em mỉm cười mỗi ngày, tặng em nụ cười để luôn rạng ngời và gửi tặng em những điều tốt đẹp nhất. Chúc em yêu Giáng sinh ấm áp và an lành.';
+
+const giftImage = 'hot-girl.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
